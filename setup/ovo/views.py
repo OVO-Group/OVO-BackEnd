cc7bf9113b20b50265c005c2bb624b8f98a00029
Terreri <100310758+Terreri@users.noreply.github.com>
2024-05-15 13:59:38 -0300

    Criação da nota fiscal

--- setup/ovo/views.py
+++ setup/ovo/views.py
@@ -13,6 +13,11 @@ from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.permissions import AllowAny
 from django.core import serializers
+from django.core.mail import send_mail
+from django.utils.html import strip_tags
+import datetime
+import random
+
 #CRUD usuário
     
 
@@ -336,7 +341,6 @@ class PedidoCreateView(APIView):
 
         id_pedido = serializer.data.get('id_pedido')
 
-
         dados = request.data
 
         id_usuario = dados.get('id_usuario')
@@ -359,6 +363,102 @@ class PedidoCreateView(APIView):
 
         id_tipo_pagamento = dados.get('id_tipo_pagamento')
         tipo_pagamento = TipoPagamento.objects.get(id_tipo_pagamento=id_tipo_pagamento)
+
+        def gerar_nota(recipient_email):
+            pedido = get_object_or_404(Pedido, id_pedido=id_pedido)
+            produtos = pedido.id_comanda.produtos
+            subject = 'Nota fiscal eletrônica'
+            html_message = f"""
+            <html>
+            <head>
+                <title>Nota fiscal eletrônica</title>
+            </head>
+            <body>
+                <h2>Detalhes</h2>
+                <table border="1">
+                    <tr>
+                        <th>Campo</th>
+                        <th>Informação</th>
+                    </tr>
+                    <tr>
+                        <th>Emitente</th>
+                        <th>{pedido.id_restaurante.nome_restaurante}</th>
+                    </tr>
+                    <tr>
+                        <th>CNPJ Emitente</th>
+                        <th>{pedido.id_restaurante.cnpj}</th>
+                    </tr>
+                    <tr>
+                        <th>Endereco Emitente</th>
+                        <th>{pedido.id_restaurante.endereco}</th>
+                    </tr>
+                    <tr>
+                        <th>Destinatário</th>
+                        <th>{pedido.id_usuario.first_name}</th>
+                    </tr>
+                    <tr>
+                        <th>CPF Destinatário</th>
+                        <th>{pedido.id_usuario.cpf}</th>
+                    </tr>
+                    <tr>
+                        <th>Data da Emissão</th>
+                        <th>{pedido.id_restaurante.nome_restaurante}</th>
+                    </tr>
+                    <tr>
+                        <th>Data de Emissão</th>
+                        <th>{datetime.datetime.now()}</th>
+                    </tr>
+                    <tr>
+                        <th>Número da NF</th>
+                        <th>{random.randint(10000, 99999)}</th>
+                    </tr>
+                </table>
+                <h2>Produtos</h2>
+                <table border="1">
+                    <tr>
+                        <th>Nome</th>
+                        <th>Preço Unitário</th>
+                        <th>Quantidade</th>
+                        <th>Valor Total do Produto</th>
+                    </tr>
+            """
+
+         
+            valor_total_compra = sum(float(produto['preco']) * int(produto['quantidade']) for produto in produtos)
+
+           
+            for produto in produtos:
+                preco_unitario = float(produto['preco'])
+                quantidade = int(produto['quantidade'])
+                valor_total_produto = preco_unitario * quantidade
+                
+                html_message += f"""
+                    <tr>
+                        <td>{produto['nome']}</td>
+                        <td>{preco_unitario:.2f}</td>
+                        <td>{quantidade}</td>
+                        <td>{valor_total_produto:.2f}</td>
+                    </tr>
+                """
+
+        
+            html_message += f"""
+                    <tr>
+                        <td colspan="3">Valor Total da Compra</td>
+                        <td>{valor_total_compra:.2f}</td>
+                    </tr>
+                </table>
+            </body>
+            </html>
+            """
+
+            plain_message = strip_tags(html_message)
+            from_email = "gabrielduartecarneiro@gmail.com"
+            send_mail(subject, plain_message, from_email, [recipient_email], html_message=html_message)
+        
+        recipient_email = usuario.email
+
+        gerar_nota(recipient_email)
 
         data = {
             'id_pedido' : id_pedido,
